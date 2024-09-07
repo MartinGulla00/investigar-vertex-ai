c360be78272bde102035a5ee0aa68717d40b654e
MartinGulla-Lightit <martin.gulla@lightit.io>
2024-09-07 14:09:33 -0300

    feat: change wording

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     vertexai.init(project=project_id, location="us-east1")
     model = GenerativeModel(model_name="gemini-1.5-flash-001", system_instruction=[
         "Only return SQL queries without any formatting, if you can't generate a SQL query with the given context and the question, return an empty string.",
-        "The returned SQL should be ready to be executed in a MySQL database.",
+        "The returned SQL should be ready to be executed in the database.",
         "You are a data analysis expert.",
         """This are the tables in the database, their columns, and the description of the columns:
         ' Table name - Column name - Description
